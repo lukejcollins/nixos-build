6994a55162aa072cbf34add80428f3d4ae28131f
Luke Collins <lukecollins@DESKTOP-1UEL4RE>
2023-08-05 22:36:30 +0100

    Add blueman and wob

--- flake.nix
+++ flake.nix
@@ -9,22 +9,19 @@
 
   outputs = { self, nixpkgs, home-manager }: {
     homeConfigurations = {
-      lukecollins = home-manager.lib.homeManagerConfiguration {
+      myUser = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages."x86_64-linux";
         modules = [
           ({ pkgs, lib, ... }: {
-            # Define user-specific details here
-            home.username = "lukecollins";
-            home.homeDirectory = "/home/lukecollins";
-            home.stateVersion = "22.05";
+            home.packages = with pkgs; [ sway waybar wob blueman ];
 
-            home.packages = with pkgs; [ sway ];
-
-            home.file.".config/sway/config".source = ./dotfiles/.config/sway/config;
-
+	   home.file.".config/sway/config".source = ./dotfiles/.config/sway/config;
+	   home.file.".config/waybar/config".source = ./dotfiles/.config/waybar/config;
+	   home.file.".config/waybar/style.css".source = ./dotfiles/.config/waybar/style.css;
+              
             home.sessionVariables = {
               WLR_NO_HARDWARE_CURSORS = "1";
-              PATH = "${pkgs.sway}/bin:$PATH";
+              PATH = "${pkgs.sway}/bin:${pkgs.waybar}/bin:${pkgs.wob}/bin:${pkgs.blueman}/bin:$PATH";
             };
 
             programs.bash = {
@@ -33,6 +30,10 @@
                 ll = "ls -l";
               };
             };
+
+            home.username = "lukecollins";
+            home.homeDirectory = "/home/lukecollins";
+            home.stateVersion = "22.05";
           })
         ];
       };
