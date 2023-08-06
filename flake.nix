11e2741e4ffbc2df6c526f75c7cf927e02066edb
lukejcollins <luke.collins@live.co.uk>
2023-08-06 10:08:39 +0100

    Fixed relative user config

--- flake.nix
+++ flake.nix
@@ -1,3 +1,4 @@
+# flake.nix
 {
   description = "A flake for building Sway with custom configuration";
 
@@ -8,15 +9,17 @@
   };
 
   outputs = { self, nixpkgs, home-manager }: {
+    # Import user-config.nix here
+    userConfig = import ./user-config.nix;
+
     homeConfigurations = {
       myUser = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages."x86_64-linux";
         modules = [
           ({ pkgs, lib, config, ... }: {
             home.packages = with pkgs; [ sway waybar wob blueman github-cli neovim foot firefox ];
-            home.username = self.userConfig.username;
+            home.username = self.userConfig.username; # Use the imported values here
             home.homeDirectory = self.userConfig.homeDirectory;
-
             home.file.".config/sway/config".source = ./dotfiles/.config/sway/config;
             home.file.".config/waybar/config".source = ./dotfiles/.config/waybar/config;
             home.file.".config/waybar/style.css".source = ./dotfiles/.config/waybar/style.css;
