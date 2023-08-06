a42c8e4ad9708bb8cf9a1f3f763b09963fdae807
lukejcollins <luke.collins@live.co.uk>
2023-08-06 19:26:45 +0100

    Add majority of system packages for sway

--- flake.nix
+++ flake.nix
@@ -1,4 +1,3 @@
-# flake.nix
 {
   description = "A flake for building Sway with custom configuration";
 
@@ -9,25 +8,42 @@
   };
 
   outputs = { self, nixpkgs, home-manager }: {
-    # Import user-config.nix here
     userConfig = import ./user-config.nix;
 
     homeConfigurations = {
       myUser = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages."x86_64-linux";
         modules = [
-          ({ pkgs, lib, config, ... }: {
-	    fonts.fontconfig.enable = true;
-            home.packages = with pkgs; [ fontconfig font-awesome_5 cantarell-fonts noto-fonts sway waybar wob blueman github-cli neovim foot firefox ];
-            home.username = self.userConfig.username; # Use the imported values here
+          ({ pkgs, lib, config, ... }: let
+            # Define Python environment with required packages
+            myPythonEnv = pkgs.python3.withPackages (ps: with ps; [
+              pynvim
+              flake8
+              pylint
+              black
+            ]);
+          in {
+            fonts.fontconfig.enable = true;
+            home.packages = with pkgs; [
+              fontconfig font-awesome_5 cantarell-fonts noto-fonts sway waybar wob
+              blueman github-cli neovim foot firefox docker zsh networkmanager
+              libfido2 swappy nodejs shellcheck go light rofi dunst xfce.thunar
+              swaylock-effects myPythonEnv libnotify
+            ];
+            home.username = self.userConfig.username;
             home.homeDirectory = self.userConfig.homeDirectory;
             home.file.".config/sway/config".source = ./dotfiles/.config/sway/config;
             home.file.".config/waybar/config".source = ./dotfiles/.config/waybar/config;
             home.file.".config/waybar/style.css".source = ./dotfiles/.config/waybar/style.css;
-	    home.file.".config/foot/foot.ini".source = ./dotfiles/.config/foot/foot.ini;
+            home.file.".config/foot/foot.ini".source = ./dotfiles/.config/foot/foot.ini;
             home.sessionVariables = {
               WLR_NO_HARDWARE_CURSORS = "1";
-              PATH = "${pkgs.sway}/bin:${pkgs.waybar}/bin:${pkgs.wob}/bin:${pkgs.blueman}/bin:${pkgs.github-cli}/bin:${pkgs.neovim}/bin:${pkgs.foot}/bin:${pkgs.firefox}/bin:$PATH";
+              PATH = with pkgs;
+                "${pkgs.sway}/bin:${pkgs.waybar}/bin:${pkgs.wob}/bin:${pkgs.blueman}/bin:${pkgs.github-cli}/bin"
+              + ":${pkgs.neovim}/bin:${pkgs.foot}/bin:${pkgs.firefox}/bin:${pkgs.docker}/bin:${pkgs.zsh}/bin"
+              + ":${pkgs.networkmanager}/bin:${pkgs.nodejs}/bin:${myPythonEnv}/bin:${pkgs.go}/bin:${pkgs.light}/bin"
+              + ":${pkgs.rofi}/bin:${pkgs.dunst}/bin:${pkgs.xfce.thunar}/bin:${pkgs.swaylock-effects}/bin"
+	      + ":${pkgs.libnotify}/bin:$PATH";
             };
 
             programs.bash = {
@@ -45,4 +61,3 @@
     };
   };
 }
-
