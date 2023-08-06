b651dc1142fb4d324b64ecc61e545806d1db2d2c
lukejcollins <luke.collins@live.co.uk>
2023-08-06 18:31:41 +0100

    Add fonts and add foot config

--- flake.nix
+++ flake.nix
@@ -17,13 +17,14 @@
         pkgs = nixpkgs.legacyPackages."x86_64-linux";
         modules = [
           ({ pkgs, lib, config, ... }: {
-            home.packages = with pkgs; [ sway waybar wob blueman github-cli neovim foot firefox ];
+	    fonts.fontconfig.enable = true;
+            home.packages = with pkgs; [ fontconfig font-awesome_5 cantarell-fonts noto-fonts sway waybar wob blueman github-cli neovim foot firefox ];
             home.username = self.userConfig.username; # Use the imported values here
             home.homeDirectory = self.userConfig.homeDirectory;
             home.file.".config/sway/config".source = ./dotfiles/.config/sway/config;
             home.file.".config/waybar/config".source = ./dotfiles/.config/waybar/config;
             home.file.".config/waybar/style.css".source = ./dotfiles/.config/waybar/style.css;
-
+	    home.file.".config/foot/foot.ini".source = ./dotfiles/.config/foot/foot.ini;
             home.sessionVariables = {
               WLR_NO_HARDWARE_CURSORS = "1";
               PATH = "${pkgs.sway}/bin:${pkgs.waybar}/bin:${pkgs.wob}/bin:${pkgs.blueman}/bin:${pkgs.github-cli}/bin:${pkgs.neovim}/bin:${pkgs.foot}/bin:${pkgs.firefox}/bin:$PATH";
