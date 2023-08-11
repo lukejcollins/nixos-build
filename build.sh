6eb03291ae1fab67d4b88771abc65a0cd00282cb
Luke <44213313+lukejcollins@users.noreply.github.com>
2023-08-11 20:39:04 +0000

    Format lint comment (#2)
    * Changed to a NixOS flake
    
    * Formatted and commented

--- build.sh
+++ build.sh
@@ -1,9 +1,34 @@
+# This script generates a Nix expression with file paths from the dotfiles directory
+# and then triggers rebuilds for NixOS and home-manager.
+
+# Generate the Nix expression:
+# 1. Start with an opening brace
+# 2. Find all files in the 'dotfiles' directory
+# 3. Strip the 'dotfiles/' prefix from each line
+# 4. Use awk to format each line into a Nix attribute set entry
+# 5. End with a closing brace
 {
-    echo "{";
+    echo "{"
+
+    # Chain together find, sed, and awk to transform the file list
     find dotfiles -type f | \
     sed 's|^dotfiles/||' | \
-    awk '{print "  \"" $0 "\".source = ./dotfiles/" $0 ";"}';
-    echo "}";
+    awk '{print "  \"" $0 "\".source = ./dotfiles/" $0 ";"}'
+
+    echo "}"
 } > generated-files.nix
+
+# Git add the generated-files.nix file temporarily
+git add -f "$(pwd)/generated-files.nix"
+git add -f "$(pwd)/hardware-configuration.nix"
+
+# Rebuild NixOS with the current directory as a flake target named 'mySystem'
 sudo nixos-rebuild switch --flake "$(pwd)#mySystem"
+
+# Rebuild home-manager with the current directory as a flake target named 'myUser'
+# and enabling extra experimental features.
 home-manager switch --flake "$(pwd)#myUser" --extra-experimental-features nix-command --extra-experimental-features flakes
+
+# Cleanup the generated-files.nix file
+git rm -f "$(pwd)/generated-files.nix"
+git rm -f "$(pwd)/hardware-configuration.nix" --cached
--- configuration.nix
+++ configuration.nix
@@ -1,36 +1,50 @@
 { config, pkgs, ... }:
 
 let
+  # Import user-specific configuration
   userConfig = import ./user-config.nix;
-in {
-  imports =
-    [
-      ./hardware-configuration.nix
-    ];
 
+in {
+  # Include hardware-specific configurations
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  # Boot settings related to systemd-boot and EFI
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # User configurations based on imported user-specific settings
   users.users.${userConfig.username} = {
-     isNormalUser = true;
-     home = userConfig.homeDirectory;
-     shell = pkgs.zsh;
-     extraGroups = [ "wheel" ];
-   };
-  
+    isNormalUser = true;
+    home = userConfig.homeDirectory;
+    shell = pkgs.zsh; # Setting Zsh as the default shell
+    extraGroups = [ "wheel" ]; # Adding the user to the wheel group for sudo privileges
+  };
+
+  # Enable ZSH for the system
   programs.zsh.enable = true;
-  
+
+  # Enable sound support
   sound.enable = true;
 
+  # Enable PulseAudio for sound management
   hardware.pulseaudio.enable = true;
- 
-  hardware.bluetooth.enable = true;
-  services.blueman.enable = true;
-  hardware.bluetooth.package = pkgs.bluezFull;
-  hardware.bluetooth.powerOnBoot = true;
 
+  # Bluetooth configurations
+  hardware.bluetooth = {
+    enable = true;
+    package = pkgs.bluezFull; # Use the full Bluez package for Bluetooth support
+    powerOnBoot = true; # Power on Bluetooth devices at boot
+  };
+
+  # Blueman service for managing Bluetooth devices from the GUI
+  services.blueman.enable = true;
+
+  # Specify the Linux kernel package version
   boot.kernelPackages = pkgs.linuxPackages_6_4;
 
+  # List of packages to be globally installed on the system
   environment.systemPackages = with pkgs; [
     alacritty
     neovim
@@ -65,12 +79,15 @@ in {
     bluez
   ];
 
-
+  # OpenGL configuration settings
   hardware.opengl = {
-    enable = true;
-    driSupport = true;
+    enable = true; # Enable OpenGL support
+    driSupport = true; # Enable Direct Rendering Infrastructure support
   };
 
+  # System version specification
   system.stateVersion = "23.05";
+
+  # Enable NetworkManager for network management
   networking.networkmanager.enable = true;
 }
