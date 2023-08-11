95d81b7575f0100a0871221a205129fab475a5ec
lukejcollins <luke.collins@live.co.uk>
2023-08-11 20:26:56 +0000

    Formatted and commented

--- flake.nix
+++ flake.nix
@@ -1,26 +1,44 @@
 {
+  # Description of the flake
   description = "A flake for building Sway with custom configuration";
 
+  # Flake input sources
   inputs = {
+    # Using the unstable branch of Nixpkgs from GitHub
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # Importing home-manager
     home-manager.url = "github:nix-community/home-manager";
+
+    # Ensure that home-manager uses the same nixpkgs as this flake
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
+  # Define the outputs of the flake
   outputs = { self, nixpkgs, home-manager }: {
+
+    # Import user-specific configuration
     userConfig = import ./user-config.nix;
 
+    # NixOS system configuration
     nixosConfigurations.mySystem = nixpkgs.lib.nixosSystem {
+      # Specify system architecture
       system = "x86_64-linux";
+
+      # List of NixOS configuration modules
       modules = [
         (import ./configuration.nix)
-	(import ./hardware-configuration.nix)
+        (import ./hardware-configuration.nix)
       ];
     };
 
+    # Home-manager user configurations
     homeConfigurations = {
       myUser = home-manager.lib.homeManagerConfiguration {
+        # Specify package set for user environment
         pkgs = nixpkgs.legacyPackages."x86_64-linux";
+
+        # List of home-manager modules for user environment
         modules = [ (import ./home.nix) ];
       };
     };
--- home.nix
+++ home.nix
@@ -1,38 +1,55 @@
 { pkgs, lib, config, ... }:
 
 let
+  # Import user-specific configuration
   userConfig = import ./user-config.nix;
+
+  # Import generated file definitions
   generatedFiles = import ./generated-files.nix;
 
-  # Define Python environment with required packages
+  # Define a Python environment with the specified packages
   myPythonEnv = pkgs.python3.withPackages (ps: with ps; [
     pynvim
     flake8
     pylint
     black
   ]);
-
-in {
+in
+{
+  # Enable fontconfig for font management
   fonts.fontconfig.enable = true;
-  
+
+  # Specify packages to be installed for the user
   home.packages = with pkgs; [
-    fontconfig font-awesome_5 cantarell-fonts noto-fonts myPythonEnv
+    fontconfig
+    font-awesome_5
+    cantarell-fonts
+    noto-fonts
+    myPythonEnv
   ];
 
+  # ZSH shell configuration
   programs.zsh = {
-    enable = true;
+    enable = true;  # Enable ZSH as the shell
+
+    # Set aliases for ZSH shell
     shellAliases = {
-      vim = "nvim";
+      vim = "nvim";  # Use 'nvim' when 'vim' is called
     };
   };
 
+  # Set user-specific details from the imported configuration
   home.username = userConfig.username;
   home.homeDirectory = userConfig.homeDirectory;
+
+  # Adjust user's PATH to include the Python environment binaries
   home.sessionVariables = {
     PATH = with pkgs; "${myPythonEnv}/bin:$PATH";
   };
-  
+
+  # Include the generated file definitions
   home.file = generatedFiles;
 
+  # Specify the state version for home-manager
   home.stateVersion = "22.05";
 }
