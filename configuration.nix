f66687a5a4fcf6c9e8be8e365ab23dafc72da183
lukejcollins <luke.collins@live.co.uk>
2023-08-22 23:27:07 +0100

    Switch to swayfx and amend config to accomodate

--- configuration.nix
+++ configuration.nix
@@ -29,7 +29,7 @@ in
   # Software and Package Configurations
   programs.zsh.enable = true; # Enable ZSH for the system
   environment.systemPackages = with pkgs; [ # List of packages to be globally installed
-    alacritty neovim sway firefox-wayland wget docker wob libfido2 gh swappy swaylock-effects
+    alacritty neovim swayfx firefox-wayland wget docker wob libfido2 gh swappy swaylock-effects
     nodejs python3 python3Packages.pip shellcheck wdisplays git waybar blueman brightnessctl
     home-manager pavucontrol alsa-utils grim bluez dconf tidal-hifi vscode gnome.gnome-boxes 
     shfmt mako slurp wl-clipboard unzip statix nixpkgs-fmt neofetch rofi
