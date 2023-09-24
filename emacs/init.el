8ed0a6ee9ae3f9e0feed5d567a6cd2247e5453ff
lukejcollins <luke.collins@live.co.uk>
2023-09-24 14:13:15 +0100

    Reformat init.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,73 +1,46 @@
 (require 'use-package)
 
-;; Solarized theme
+;; Theme
 (use-package solarized-theme
   :ensure t
   :config
   (load-theme 'solarized-light t))
 
-;; Change GNU logo
+;; Startup appearance
 (setq fancy-splash-image "~/Pictures/gnu_color.png")
-
-;; Use Treemacs
-(use-package treemacs
-  :ensure t
-  :config
-  (use-package treemacs-all-the-icons
-    :ensure t)
-
-  ;; Keybindings (customize as you prefer)
-  :bind
-  (("C-x t" . treemacs))
-
-  ;; Configure icons (if you have treemacs-all-the-icons installed)
-  :custom
-  (treemacs-theme "all-the-icons"))
-
-;; Use Helm
-(use-package helm
-  :ensure t
-  :config
-  (helm-mode 1)
-  :bind
-  (("M-x" . helm-M-x)))
-
-;; Use Vterm
-(use-package vterm
-  :ensure t
-  :config
-  ;; example of setting a vterm option
-  (setq vterm-max-scrollback 5000))
 
 ;; Clipboard Configuration
 (setq select-enable-clipboard t
       select-enable-primary t)
 
-;; Terraform Mode for syntax highlighting
-(use-package terraform-mode :ensure t)
-
-;; Dockerfile Mode for syntax highlighting
-(use-package dockerfile-mode :ensure t)
-
-;; Nix Mode for syntax highlighting
-(use-package nix-mode :ensure t)
-
-;; Flycheck Configuration for linting
-(use-package flycheck
-  :ensure t
-  :init (global-flycheck-mode))
-
 ;; Disable tooltip mode
 (tooltip-mode -1)
 
-;; Flycheck Inline Configuration
-(use-package flycheck-inline
+;; Helm configuration
+(use-package helm
   :ensure t
-  :after flycheck
-  :hook (flycheck-mode . flycheck-inline-mode))
+  :config (helm-mode 1)
+  :bind (("M-x" . helm-M-x)))
 
-;; Set specific checkers for each mode
+;; Treemacs configuration
+(use-package treemacs
+  :ensure t
+  :bind (("C-x t" . treemacs))
+  :custom (treemacs-theme "all-the-icons")
+  :config
+  (use-package treemacs-all-the-icons
+    :ensure t))
+
+;; Vterm configuration
+(use-package vterm
+  :ensure t
+  :config
+  (setq vterm-max-scrollback 5000))
+
+;; Linting with Flycheck
 (use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode)
   :config
   (setq-default flycheck-disabled-checkers '(python-pycompile))
   (add-hook 'python-mode-hook (lambda () (flycheck-select-checker 'python-flake8)))
@@ -76,14 +49,24 @@
   (add-hook 'nix-mode-hook (lambda () (flycheck-select-checker 'nix-statix)))
   (add-hook 'terraform-mode-hook (lambda () (flycheck-select-checker 'terraform-tflint))))
 
-;; Blacken Configuration for Python
+(use-package flycheck-inline
+  :ensure t
+  :after flycheck
+  :hook (flycheck-mode . flycheck-inline-mode))
+
+;; Modes for various file types
+(use-package terraform-mode :ensure t)
+(use-package dockerfile-mode :ensure t)
+(use-package nix-mode :ensure t)
+
+;; Python configuration
 (use-package blacken
   :ensure t
   :hook (python-mode . blacken-mode)
   :config
   (setq blacken-line-length 79))
 
-;; Bind F5 to format buffer
+;; Formatting keybinding
 (global-set-key (kbd "<f5>") (lambda ()
                                (interactive)
                                (cond ((eq major-mode 'python-mode) (blacken-buffer))
