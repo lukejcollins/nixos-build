2121fd8d9477ffeba90f3151ace04dc1304f5b36
lukejcollins <luke.collins@live.co.uk>
2023-10-02 22:21:12 +0100

    Change to btop and add postgresql

--- configuration.nix
+++ configuration.nix
@@ -42,7 +42,7 @@ in
     alacritty neovim google-chrome wget docker wob libfido2 gh swappy swaylock-effects
     nodejs python3 python3Packages.pip shellcheck wdisplays git blueman brightnessctl hyprpaper
     home-manager pavucontrol alsa-utils grim bluez vscode gnome.gnome-boxes shfmt mako slurp 
-    wl-clipboard unzip statix nixpkgs-fmt neofetch rofi-wayland libnotify waybar gotop
+    wl-clipboard unzip statix nixpkgs-fmt neofetch rofi-wayland libnotify waybar btop postgresql
     (emacsWithPackagesFromUsePackage {
       config = ./emacs/init.el;
       defaultInitFile = true;
